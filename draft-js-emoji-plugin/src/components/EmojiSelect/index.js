568dbf82e054350601090c82233d2bda0adeae1a
Ryan McClure <rmcclure@reachify.io>
2018-04-30 08:14:41 -0700

    prevent popover from loading until open

--- draft-js-emoji-plugin/src/components/EmojiSelect/index.js
+++ draft-js-emoji-plugin/src/components/EmojiSelect/index.js
@@ -103,18 +103,20 @@ export default class EmojiSelect extends Component {
         >
           {selectButtonContent}
         </button>
-        <Popover
-          cacheBustParam={cacheBustParam}
-          imagePath={imagePath}
-          imageType={imageType}
-          theme={theme}
-          store={store}
-          groups={selectGroups}
-          emojis={emojis}
-          toneSelectOpenDelay={toneSelectOpenDelay}
-          isOpen={this.state.isOpen}
-          useNativeArt={useNativeArt}
-        />
+        {this.state.isOpen ?
+          <Popover
+            cacheBustParam={cacheBustParam}
+            imagePath={imagePath}
+            imageType={imageType}
+            theme={theme}
+            store={store}
+            groups={selectGroups}
+            emojis={emojis}
+            toneSelectOpenDelay={toneSelectOpenDelay}
+            isOpen={this.state.isOpen}
+            useNativeArt={useNativeArt}
+          />
+	: false}
       </div>
     );
   }
